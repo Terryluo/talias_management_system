9c3417592b3fda97a14a87b25a57f8da99d57182
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2023-11-14 15:46:56 -0500

    basic framework of the project

---
+++ backend/talias-web-management/src/main/java/com/twm/taliaswebmanagement/TaliasWebManagementApplication.java
@@ -0,0 +1,13 @@
+package com.twm.taliaswebmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaliasWebManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaliasWebManagementApplication.class, args);
+    }
+
+}
---
+++ backend/talias-web-management/src/test/java/com/twm/taliaswebmanagement/TaliasWebManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twm.taliaswebmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaliasWebManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
