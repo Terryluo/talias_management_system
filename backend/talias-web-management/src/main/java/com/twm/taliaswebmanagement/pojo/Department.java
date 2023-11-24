66f3d6f3bdd8858c731fa0aef421c7b5722c924e
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2023-11-24 09:27:07 -0500

    pojo for the Department.

---
+++ backend/talias-web-management/src/main/java/com/twm/taliaswebmanagement/pojo/Department.java
@@ -0,0 +1,10 @@
+package com.twm.taliaswebmanagement.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+    int id;
+}
